a0555e3748b6fb80ace182ea7ae8f4f8525d0f5b
黄炼 <huanglian684@gmail.com>
2024-12-23 15:44:31 +0800

    新增处理工作日志方法

---
+++ src/main/java/test/NumberTest.java
@@ -0,0 +1,18 @@
+package test;
+
+/**
+ * Title: <br/>
+ * Description: <br/>
+ * Copyright: 2024 <br/>
+ * Company:<br/>
+ * Project: design-pattern <br/>
+ *
+ * @Author huanglian <br/>
+ * Create Time:11/20/24 15:27 <br/>
+ */
+public class NumberTest {
+    public static void main(String[] args) {
+        String s = String.valueOf(052761);
+        System.out.println(s);
+    }
+}
